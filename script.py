8f01c524cea06a3c93fa3f08e3e04e3620d77468
Glas <48269777+DJTOMATO@users.noreply.github.com>
2023-11-28 18:06:34 -0300

    Create script.py

---
+++ script.py
@@ -0,0 +1,50 @@
+# BE SURE TO REPLACE F: WITH YOUR USB PATH
+
+py
+import os
+import shutil
+
+# Define the origin folder
+origin_folder = "F:/ROMS"
+
+# Define the destination folders for each prefix
+destinations = {
+    'nes': os.path.join("F:/", 'FC'),
+    'snes': os.path.join("F:/", 'SFC'),
+    'pce': os.path.join("F:/", 'SFC'),
+    'fds': os.path.join("F:/", 'FC'),
+    # Pending 'colecovision': os.path.join("F:/", 'FC'),
+    'a26': os.path.join("F:/", 'GB'),
+    '5200': os.path.join("F:/", 'GB'),
+    # Pending 'lynx': os.path.join("F:/", 'GB'),
+    # Pending 'intellivision': os.path.join("F:/", 'GB'),
+    'gb': os.path.join("F:/", 'GBC'),
+    'gbc': os.path.join("F:/", 'GBC'),
+    'gba': os.path.join("F:/", 'GBC'),
+    'pokem': os.path.join("F:/", 'GBC'),
+    'neo': os.path.join("F:/", 'GBA'),
+    # Pending 'wonderswan': os.path.join("F:/", 'GBA'),
+    # Pending 'supervision': os.path.join("F:/", 'GBA'),
+    'amstrad': os.path.join("F:/", 'GBA'),
+    'sega': os.path.join("F:/", 'MD'),
+    'mega': os.path.join("F:/", 'MD'),
+    'sms': os.path.join("F:/", 'MD'),
+    'game': os.path.join("F:/", 'MD'),
+    'm2k': os.path.join("F:/", 'ARCADE')
+}
+
+# Function to move files based on their prefixes
+def move_files():
+    for root, _, files in os.walk(origin_folder):
+        for file in files:
+            if file.endswith('.gba') and file.startswith(('nes', 'snes', 'pce', 'fds', 'colecovision', 'a26', '5200', 'lynx', 'intellivision', 'gb', 'gbc', 'gba', 'pokem', 'neo', 'wonderswan', 'supervision', 'amstrad', 'sega', 'mega', 'sms', 'game', 'm2k')):
+                prefix = file.split(';')[0]
+                destination = destinations.get(prefix.lower())
+                if destination:
+                    source_path = os.path.join(root, file)
+                    dest_path = os.path.join(destination, file)
+                    shutil.move(source_path, dest_path)
+                    print(f"Moved {file} to {destination}")
+
+# Call the function to move files
+move_files()
